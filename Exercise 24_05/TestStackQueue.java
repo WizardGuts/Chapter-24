121f76972d82323879b7b3be326610b47052e8d1
WizardGuts <115489852+WizardGuts@users.noreply.github.com>
2023-06-23 13:31:38 -0600

    Add files via upload

---
+++ Exercise 24_05/TestStackQueue.java
@@ -0,0 +1,19 @@
+public class TestStackQueue {
+	public static void main(String[] args) {
+		GenericQueue<String> queue = new GenericQueue<>();
+		
+		queue.enqueue("Tom");
+		System.out.println("(1) " + queue);
+		
+		queue.enqueue("Susan");
+		System.out.println("(2) " + queue);
+		
+		queue.enqueue("Kim");
+		queue.enqueue("Michael");
+		System.out.println("(3) " + queue);
+		
+		System.out.println("(4) " + queue.dequeue());
+		System.out.println("(5) " + queue.dequeue());
+		System.out.println("(6) " + queue);
+	}
+}
